2b6d6ffc4e10e3eda6abdf538546425d9c22a33d
gaurav <gauravg77061@gmail.com>
2025-09-03 00:46:06 +0530

    http request exploring get post ,delete ,post etc

--- src/app.js
+++ src/app.js
@@ -1,15 +1,32 @@
 const express=require('express');
 const app=express();
 
+// aggar mein specific call deta hu toh vo  specific http request handle karega
 
+// ye wala run karne se sirf ye hi chale baaki waale post get delete nahi chalenge kyuki ye sabke liye validd ar ye maater karta h
 
+// app.use('/user',(req,res)=>{
+//  res.send("HAHAHAHAH")
+// })
 
+app.get('/user',(req,res)=>{
+    res.send({firstName : 'Gaurav ' , LastName : 'kummar'})
+})
+
+app.post('/user',(req,res)=>{
+    res.send("Data save succesfully in DB!")
+})
+//delete http request
+app.delete('/user',(req,res)=>{
+    res.send("delted successfully");
+})
+// this wll match all the http calls 
 app.use('/test',(req,res) =>{
     res.send("server is running fron test route");
 })
-app.use((req,res)=>{
-    res.send ("hello this coming from port 3000");
-});
+// app.use((req,res)=>{
+//     res.send ("hello this coming from port 3000");
+// });
 
 
 
