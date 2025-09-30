7dde5afb7715e9852e8f425feb40680c76c956e6
gaurav <gauravg77061@gmail.com>
2025-09-30 08:59:24 +0530

    feed api correct working code

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,6 +1,7 @@
 const  express= require('express');
 const { userAuth } = require('../middlleware/auth');
 const ConnectionRequest = require('../models/connectionRequest');
+const User = require('../models/user');
 const userRouter=express.Router();
 
 const safeData= ["firstName","lastName","age","skills","about"];
@@ -66,6 +67,85 @@ userRouter.get('/connections',userAuth, async(req,res)=>{
         });
     }
 })
+
+
+userRouter.get('/feed',userAuth,async(req,res)=>{
+    try {
+        
+        const loggedInUser=req.user;
+
+        const connectRequest=await ConnectionRequest.find({
+            $or:[
+                {fromUserId : loggedInUser._id},
+                {toUserId : loggedInUser._id}
+            ]
+        }).select("fromUserId  toUserId");
+
+        const hideAllUser = new Set();
+
+        connectRequest.forEach(element => {
+            hideAllUser.add(element.fromUserId.toString());
+            hideAllUser.add(element.toUserId.toString());
+        });
+
+        console.log(hideAllUser);
+
+        const users=await User.find({
+            $and:[
+                {_id:{$nin:Array.from(hideAllUser)}},
+//isko hata kar run karna h ek baar
+
+               {_id:{$ne:loggedInUser._id}},
+            ]
+        }).select(safeData);
+
+        res.send(users);
+
+    } catch (error) {
+        res.status(400).json({
+            message:error.message
+        })
+    }
+})
+
+// userRouter.get('/feed', userAuth, async (req, res) => {
+//   try {
+//     const loggedInUser = req.user;
+
+//     // 1️⃣ Fetch all connection requests where logged-in user is involved
+//     const connectRequest = await ConnectionRequest.find({
+//       $or: [
+//         { fromUserId: loggedInUser._id },
+//         { toUserId: loggedInUser._id }
+//       ]
+//     }).select("fromUserId toUserId");
+
+//     // 2️⃣ Build a set of all users to hide (already connected or requested)
+//     const hideAllUser = new Set();
+
+//     connectRequest.forEach(element => {
+//       // Add the other user only (ignore logged-in user itself)
+//       if (element.fromUserId.toString() !== loggedInUser._id.toString())
+//         hideAllUser.add(element.fromUserId.toString());
+//       if (element.toUserId.toString() !== loggedInUser._id.toString())
+//         hideAllUser.add(element.toUserId.toString());
+//     });
+
+//     // 3️⃣ Fetch all users who are NOT in hideAllUser and NOT self
+//     const users = await User.find({
+//       _id: { $in: Array.from(hideAllUser).concat(loggedInUser._id.toString()) }
+//     }).select(safeData);
+
+//     res.send(users);
+
+//   } catch (error) {
+//     res.status(400).json({
+//       message: error.message
+//     });
+//   }
+// });
+
+
 
 module.exports =userRouter;
 
